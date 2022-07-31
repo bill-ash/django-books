61b2bd080f87947366081c23e59458703f718b0e
bill <bill@overco.net>
2022-07-30 21:58:52 -0400

    Rebuild example apps

--- examples/expense/qbwc/urls.py
+++ examples/expense/qbwc/urls.py
@@ -1,4 +1,4 @@
-"""expense URL Configuration
+"""qbwc URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('django_books.urls')),
+    path('', include('django_books.urls')), 
+    path('', include('expenses.urls')),
 ]
